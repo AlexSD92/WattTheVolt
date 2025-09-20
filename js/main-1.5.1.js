b0908b7cd099ea8b1a0a5cf1ba4d901343022acd
AlexSD92 <alex.spara.dominguez@gmail.com>
2025-09-20 13:08:12 +0100

    Prettier formatter added

--- js/main-1.5.1.js
+++ js/main-1.5.1.js
@@ -14,9 +14,7 @@ if (postList) {
             file: '4-back-on-the-bandwagon.html',
             title: 'Back on the Bandwagon'
         },
-        {file: '5-life-lately.html',
-            title: 'Life Lately'
-        },
+        { file: '5-life-lately.html', title: 'Life Lately' }
     ];
 
     posts.forEach((post) => {
